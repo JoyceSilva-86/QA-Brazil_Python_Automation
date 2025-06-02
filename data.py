9b70fb9403d48590173f4bdab1518aee9113609b
JoyceSilva-86 <joycegpereira@hotmail.com>
2025-06-02 13:59:33 +0100

    colocada a URL do servidor

--- data.py
+++ data.py
@@ -1,5 +1,5 @@
 # Arquivo com constantes
-URBAN_ROUTES_URL = ''
+URBAN_ROUTES_URL = 'https://cnt-45b19d89-6e6e-41bf-a5d0-390e9057f98d.containerhub.tripleten-services.com?lng=pt'
 ADDRESS_FROM = 'East 2nd Street, 60'
 ADDRESS_TO = '1300 1st St'
 PHONE_NUMBER = '+1 123 123 12 12'
